1f37e0a971f070b16b24c003d0ba7a36523e2773
itsABHIROOP <84619398+itsAbhirup@users.noreply.github.com>
2021-10-05 22:16:28 +0530

    Create add.c

---
+++ add.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+void main()
+{
+	float num1,num2;
+	printf("Enter two numbers :: ");
+	scanf("%f%f",&num1,&num2);
+	printf("The sum of given two numbers is :: %f\n",num1+num2);
+}
